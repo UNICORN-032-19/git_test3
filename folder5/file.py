b8ee4fcef3892d5f8803064252220efc727d0788
irina <iris_9@inbox.ru>
2020-02-07 23:59:05 +0300

    3) Change 2nd line in the file folder5/file.py

--- folder5/file.py
+++ folder5/file.py
@@ -1,5 +1,5 @@
 The Hello World project is a time-honored tradition in computer programming. It$
-You’ll learn how to:
+
     Create and use a repository
     Start and manage a new branch
     Make changes to a file and push them to GitHub as commits
