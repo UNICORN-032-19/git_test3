a61194f511fc7c3d62baef60333e4d3b97811a2e
irina <iris_9@inbox.ru>
2020-02-07 23:57:38 +0300

    2) Add new lines in the file folder5/file.py

--- folder5/file.py
+++ folder5/file.py
@@ -4,5 +4,7 @@ You’ll learn how to:
     Start and manage a new branch
     Make changes to a file and push them to GitHub as commits
     Open and merge a pull request
-
+What is GitHub?
+GitHub is a code hosting platform for version control and collaboration.
+It lets you and others work together on projects from anywhere.
 
